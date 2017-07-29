7592917cfd34fe6a69b3e9c64b2cb543d70608d3
molecular <molecular@0x0000.de>
2017-07-29 02:06:22 +0200

    fix bug in do_filter_dump.sh and remove DIR from config.sh

--- config_example.sh
+++ config_example.sh
@@ -1,4 +1,3 @@
 # example config, rename to "config.sh" and adjust
-export DIR=/home/user/bitcoin-seeder
 export FILTER_PORT=8333
 export SCP_DEST="user@remote.host:python-seeder/zones/myseeder.mydomain"
--- do_filter_dump.sh
+++ do_filter_dump.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+cd $(dirname $0)
 source ./config.sh
-if [[ "$DIR" == "" ]]; then
-	DIR=$(pwd);
-fi
-cd $DIR
 tail -n +2 dnsseed.dump | ./filter_dump.sh > ipv4_nodes.dat && \
 scp ipv4_nodes.dat $SCP_DEST
 
