c0e6d3c736a767aa969ccc5947f171912b671300
molecular <molecular@0x0000.de>
2017-07-29 02:14:01 +0200

    fix bug in filter_dump.sh

--- filter_dump.sh
+++ filter_dump.sh
@@ -7,7 +7,7 @@ echo "" > dnsseed.filtered.dump
 while IFS=' ' read -r host good lastSuccess p2h p8h p1d p7d p30d blocks svcs version subver || [[ -n "$host" ]]; do
 	port=$(echo $host | cut -d : -f 2)
 	host=$(echo $host | cut -d : -f 1)
-	if [[ "$port" -eq "18333" ]]; then
+	if [[ "$port" -eq "$FILTER_PORT" ]]; then
 		if (((16#$svcs & 16#20) != 0)); then # service bit BITCOIN_CASH 1 << 5
 			echo $host:$port $good $lastSuccess $p2h $p8h $p1d $p7d $p30d $blocks $svcs $version $subver >> dnsseed.filtered.dump 
 			echo $host
