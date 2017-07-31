901dd52143f006a1e1244b774a053a6968df0f8e
molecular <molec@posteo.de>
2017-07-31 19:06:23 +0200

    * change version string in main.cpp  * prioritize nodes by 1.) good flag 2.) ABC 0.14.6  * introduce configurable MAX_NODES limit (to avoid some dns problems with too many records)

--- config_example.sh
+++ config_example.sh
@@ -1,3 +1,5 @@
 # example config, rename to "config.sh" and adjust
 export FILTER_PORT=8333
 export SCP_DEST="user@remote.host:python-seeder/zones/myseeder.mydomain"
+export MAX_NODES=200 # limit number of nodes to output
+export OUT=out # temp-folder for prioritizing files
--- do_filter_dump.sh
+++ do_filter_dump.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 cd $(dirname $0)
 source ./config.sh
-tail -n +2 dnsseed.dump | ./filter_dump.sh > ipv4_nodes.dat && \
-scp ipv4_nodes.dat $SCP_DEST
 
+# feed into filter scripts
+tail -n +2 dnsseed.dump | ./filter_dump.sh || exit 0
+
+# prep / clear output files
+> ipv4_nodes.dat
+head -n 1 dnsseed.dump > dnsseed.filtered.dump
+
+# concatenate files from $OUT folder
+for i in 0 1 2; do
+	cat $OUT/$i.ipv4 >> ipv4_nodes.dat
+	cat $OUT/$i.filtered.dump >> dnsseed.filtered.dump
+done 
+
+# truncate to MAX_NODES
+mv ipv4_nodes.dat ipv4_nodes.full.dat
+head -n $MAX_NODES ipv4_nodes.full.dat > ipv4_nodes.dat
+
+# copy to dns server
+scp ipv4_nodes.dat $SCP_DEST
